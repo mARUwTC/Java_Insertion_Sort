eb570a1531514f99ea642eb4c67dce9966e1cc5a
mARUwTC <86964487+mARUwTC@users.noreply.github.com>
2022-03-29 17:23:32 +0800

    Update and rename Sorting_number_unlimited.java to Insertion_Sort.java

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -1,5 +1,5 @@
 import java.io.*;
-public class Sorting_number_unlimited{
+public class Insertion_Sort{
     public static void insertionSort(int array[]){
         int n = array.length;  
         for (int j=1; j<n; j++) {  
