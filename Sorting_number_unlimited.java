152162a43e3034341b099f80f84a1d8793812c07
mARUwTC <86964487+mARUwTC@users.noreply.github.com>
2022-03-29 17:20:30 +0800

    Add files via upload

---
+++ Sorting_number_unlimited.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class Sorting_number_unlimited{
+    public static void insertionSort(int array[]){
+        int n = array.length;  
+        for (int j=1; j<n; j++) {  
+            int k=array[j];  
+            int i=j-1;
+            for (;(i>-1)&&(array[i]>k);i--){
+                array[i+1]=array[i];
+            }
+            array[i+1]=k; 
+        } 
+    }
+    public static void main (String[] args) throws Exception {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("How many integers to be input: ");
+        int count = Integer.parseInt(buf.readLine());
+
+        int[] num = new int[count];
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        for (int i=0; i<count; i++){
+        System.out.print("Enter the " + (i+1) + " numbers: ");
+        num[i] = Integer.parseInt(buff.readLine()); }
+        insertionSort(num);
+        for (int j=0; j<num.length; j++) {
+            System.out.println("The " + (j+1) + "-th smallest integer is: " + num[j]);
+        }
+    }
+}
